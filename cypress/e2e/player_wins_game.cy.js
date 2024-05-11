dc5fc1cd081e982e7d648cc2e81630ae1df65531
Kilian Sprotte <kilian.sprotte@gmail.com>
2024-05-11 14:50:35 +0200

    cypress/e2e/player_wins_game.cy.js

--- cypress/e2e/player_wins_game.cy.js
+++ cypress/e2e/player_wins_game.cy.js
@@ -1,5 +1,5 @@
 describe('Player wins game', () => {
-  describe('when winning on a row', () => {
+  describe('when X wins on a row', () => {
     it('a winning message is shown', () => {
       cy.visit('/')
 
@@ -28,12 +28,41 @@ describe('Player wins game', () => {
         .should('be.visible')
         .and('have.text', 'The winner is X!')
 
-      cy.get('[data-testid="cell"]').eq(3).should('have.text', '')
-
       cy.wait(2000)
+
       cy.get('[data-testid="game-ends-message"]')
         .should('be.visible')
         .and('have.text', 'The winner is X!')
+    })
+
+    it('player O will make no more moves', () => {
+      cy.visit('/')
+
+      cy.get('[data-testid="start-new-game-button"]').click()
+
+      cy.get('[data-testid="cell"]').eq(0).should('have.text', '')
+
+      cy.get('[data-testid="game-ends-message"]').should('not.exist')
+
+      cy.window().invoke('setBoardModel', [
+        'X',
+        'X',
+        null,
+        null,
+        'O',
+        null,
+        'O',
+        null,
+        null,
+      ])
+
+      cy.get('[data-testid="cell"]').eq(2).click()
+      cy.get('[data-testid="cell"]').eq(2).should('have.text', 'X')
+
+      cy.get('[data-testid="cell"]').eq(3).should('have.text', '')
+
+      cy.wait(2000)
+
       cy.get('[data-testid="cell"]').eq(3).should('have.text', '')
     })
   })
